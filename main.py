f52f63abad95162c390718edc9b5dbc2c0a99125
Max Garin <maxgarin@gmail.com>
2018-11-03 00:22:15 -0400

    Added login and topic parsing for reddit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import requests
+import praw
+
+reddit = praw.Reddit(client_id='jXCPXiuviPOGHA', client_secret='WTxSVdlLC5ZjWXhBLvQ2qTkkhCk', username='livestreamstwitter', password='cookies', user_agent='linux:livestreamtwitter:v1.0 (by /u/sorieus')
+
+subreddit = reddit.subreddit('livestreamfail')
+
+top_livestreamfail = subreddit.top(time_filter='month' ,limit=25)
+
+for submission in top_livestreamfail:
+    if not submission.stickied:
+        print(f'Title: {submission.title}\n\n Url:{submission.url}\n')
